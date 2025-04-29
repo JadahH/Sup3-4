affbd6ae0133bc0a2aa8e087b1b06cd51ce36baf
JadahH <Jhol1918@students.vsu.edu>
2025-04-28 21:57:21 -0400

    implemented function

--- src/prime/Prime_num.py
+++ src/prime/Prime_num.py
@@ -36,3 +36,9 @@ def subrange(rng: tuple[int, int]) -> list[int]:
     sub_start, sub_end = rng
     return [n for n in range(sub_start, sub_end + 1) if if_prime(n)]
 
+def run_tests():
+    loader = unittest.TestLoader()
+    suite = loader.loadTestsFromModule(sys.modules[__name__])
+    runner = unittest.TextTestRunner(verbosity=2)
+    result = runner.run(suite)
+    sys.exit(0 if result.wasSuccessful() else 1)
