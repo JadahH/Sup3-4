6b6dd67e30c6a104c7dcec999859aac7906b41d7
JadahH <Jhol1918@students.vsu.edu>
2025-04-28 21:33:25 -0400

    implemented test for range_split

--- tests/tests_prime.py
+++ tests/tests_prime.py
@@ -1,4 +1,4 @@
-from prime import if_prime 
+from prime import if_prime, range_split
 
 def test_if_prime_small(self):
         self.assertFalse(if_prime(0))
@@ -7,3 +7,8 @@ def test_if_prime_small(self):
         self.assertTrue(if_prime(3))
         self.assertFalse(if_prime(4))
         self.assertTrue(if_prime(13))
+
+def test_split_range_even(self):
+        rngs = range_split(1, 10, 5)
+        # total 10 numbers, 5 chunks → each size 2
+        self.assertEqual(rngs, [(1,2),(3,4),(5,6),(7,8),(9,10)])
